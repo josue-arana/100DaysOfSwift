a1f413ac5a40027519eb642f7d51e230e1d7d3bc
Josue Arana <josuearana@josues-mbp.mynetworksettings.com>
2022-02-23 23:13:23 -0500

    Day 27

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Josue Arana on 2/22/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
