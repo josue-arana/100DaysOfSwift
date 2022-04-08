234c20e6e281385528de8215a1d666a0fdb6695c
Josue <josuee.arana@gmail.com>
2022-04-08 19:26:44 -0400

    Day 43, Drawing

---
+++ Drawing/Drawing/ContentView.swift
@@ -0,0 +1,78 @@
+//
+//  ContentView.swift
+//  Drawing
+//
+//  Created by Josue on 4/8/22.
+//
+
+import SwiftUI
+
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        
+        return path
+    }
+}
+
+struct Arc: InsettableShape {
+    
+    let startAngle: Angle
+    let endAngle: Angle
+    let clockwise: Bool
+    var insetAmount = 0.0
+    
+    func path(in rect: CGRect) -> Path {
+        
+        let rotationAdjustment = Angle.degrees(90)
+        let modifiedStart = startAngle - rotationAdjustment
+        let modifiedEnd = endAngle - rotationAdjustment
+        
+        var path = Path()
+        
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
+        
+        return path
+    }
+    
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        var arc = self
+        arc.insetAmount += amount
+        return arc
+    }
+    
+}
+
+struct ContentView: View {
+    var body: some View {
+        
+        
+        
+        Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
+            .strokeBorder(.blue, lineWidth: 40)
+//            .frame(width: 300, height: 300)
+        
+        
+//        Triangle()
+//            .stroke(.red, style: StrokeStyle(lineWidth: 15, lineCap: .round, lineJoin: .round))
+//            .frame(width: 300, height: 300)
+//        Path { path in
+//            path.move(to: CGPoint(x: 200, y: 100))
+//            path.addLine(to: CGPoint(x: 100, y: 300))
+//            path.addLine(to: CGPoint(x: 300, y: 300))
+//            path.addLine(to: CGPoint(x: 200, y: 100))
+//        }
+//        .stroke(.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Josue on 4/8/22.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
