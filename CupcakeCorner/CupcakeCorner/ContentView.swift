72d8104b3ca5ebc57e40f738dac7c108d3b1ae65
Josue <josuee.arana@gmail.com>
2022-04-26 23:53:08 -0400

    49

---
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by Josue on 4/26/22.
+//
+
+import SwiftUI
+
+class User: ObservableObject, Codable {
+    enum CodingKeys: CodingKey {
+        case name
+    }
+    
+    @Published var name = "Josue Arana"
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+    }
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name )
+    }
+}
+
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
