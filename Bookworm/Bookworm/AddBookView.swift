3590bbda4b3e4b20558f823f84b82af87a27518b
Josue <josuee.arana@gmail.com>
2022-06-08 22:35:38 -0400

    Day 54 and 55

--- Bookworm/Bookworm/AddBookView.swift
+++ Bookworm/Bookworm/AddBookView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct AddBookView: View {
     @Environment(\.managedObjectContext) var moc
+    @Environment(\.dismiss) var dismiss
+    
     @State private var title = ""
     @State private var author = ""
     @State private var rating = 3
@@ -29,23 +31,29 @@ struct AddBookView: View {
                             Text($0)
                         }
                     }
+                    
                 } // end section
                 
                 Section {
                     TextEditor(text: $review)
                     
-                    Picker("Rating", selection: $rating) {
-                        ForEach(0..<6){
-                            Text(String($0))
-                        }
-                    }
+                    RatingView(rating: $rating)
                 } header: {
                     Text("Write a review")
                 }
                 
                 Section {
                     Button("Save"){
-                        //add the book
+                        let newBook = Book(context: moc)
+                        newBook.id = UUID()
+                        newBook.title = title
+                        newBook.author = author
+                        newBook.rating = Int16(rating)
+                        newBook.genre = genre
+                        newBook.review = review
+                        
+                        try? moc.save()
+                        dismiss()
                     }
                 }
             }//end form
---
+++ Bookworm/Bookworm/EmojiRatingView.swift
@@ -0,0 +1,33 @@
+//
+//  EmojiRatingView.swift
+//  Bookworm
+//
+//  Created by Josue on 6/8/22.
+//
+
+import SwiftUI
+
+struct EmojiRatingView: View {
+    let rating: Int16
+    
+    var body: some View {
+        switch rating {
+        case 1:
+            return Text("☹️")
+        case 2:
+            return Text("😔")
+        case 3:
+            return Text("😐")
+        case 4:
+            return Text("😊")
+        default:
+            return Text("🤩")
+        }
+    }
+}
+
+struct EmojiRatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiRatingView(rating: 5)
+    }
+}
